5f41027dbdeb5c16d30abaa262948958f9496173
Cyril-ruidong <Cyril-ruidong@users.noreply.github.com>
2026-06-06 14:59:07 +0000

    feat: 开发 Cocos 3D H5 游戏
    Co-authored-by: traeagent <traeagent@users.noreply.github.com>

--- src/tilemap.js
+++ src/tilemap.js
@@ -5,11 +5,30 @@ const TILE_SIZE = 16;
 const FC_COLORS = {
   BG: '#1c3c2c',
   GRASS: '#4c8c4c',
+  GRASS_LIGHT: '#5a9a5a',
+  GRASS_DARK: '#3a7a3a',
   ROAD: '#dcc89c',
+  ROAD_DARK: '#c8b080',
+  ROAD_LIGHT: '#ecd8ac',
   BUILDING: '#ac7c5c',
+  BUILDING_DARK: '#8c5c3c',
+  BUILDING_LIGHT: '#cc9c7c',
+  ROOF: '#cc4444',
+  ROOF_DARK: '#aa2222',
+  ROOF_LIGHT: '#ee6666',
   WATER: '#5c9cbc',
+  WATER_LIGHT: '#7cbcdc',
+  WATER_DARK: '#4c7c9c',
   BLACK: '#000000',
-  WHITE: '#fcfffc'
+  WHITE: '#fcfffc',
+  YELLOW: '#f8f880',
+  RED: '#f88080',
+  WINDOW: '#3c5c7c',
+  TREE_TRUNK: '#6c4c2c',
+  TREE_LEAVES: '#2c6c2c',
+  TREE_LIGHT: '#3c8c3c',
+  STONE: '#9c9c9c',
+  STONE_DARK: '#7c7c7c'
 };
 
 const TILES = {
@@ -22,7 +41,10 @@ const TILES = {
   DOOR: 6,
   ROOF: 7,
   TREE: 8,
-  FENCE: 9
+  FENCE: 9,
+  STONE: 10,
+  FLOWER: 11,
+  PATH: 12
 };
 
 const TileSprites = {
@@ -32,212 +54,304 @@ const TileSprites = {
   drawGrass(ctx, x, y) {
     PixelSprites.drawSpriteFromGrid(ctx, [
       'GGGGGGGGGGGGGGGG',
+      'GGLGGGGGGGGFGGGG',
       'GGGGGGGGGGGGGGGG',
-      'GGGBGGGGGGGGGGGG',
-      'GGGGGGGGGGGGGGGG',
-      'GGGGGGGGGGGGGGGG',
-      'GGGGGGGGGBGGGGGG',
+      'GGGGFGGGGGGGGGGG',
       'GGGGGGGGGGGGGGGG',
       'GGGGGGGGGGGGGGGG',
+      'GGGGGGGGGGFGGGGG',
+      'GGGGGGGGGGGGGGGG',
+      'GGLGGGGGGGGGGGGG',
+      'GGGGGGGGGGGGFGGG',
+      'GGGGGGGGGGGGGGGG',
+      'GGGGFGGGGGGGGGGG',
       'GGGGGGGGGGGGGGGG',
       'GGGGGGGGGGGGGGGG',
-      'GGGGGGGGGGGGGGGG',
-      'GGGGGBGGGGGGGGGG',
-      'GGGGGGGGGGGGGGGG',
-      'GGGGGGGGGGGGGGGG',
-      'GGGGGGGGGGGGGGGG',
+      'GGGGGGFGGGGGGGGG',
       'GGGGGGGGGGGGGGGG'
-    ], x, y, 1, { G: FC_COLORS.GRASS, B: FC_COLORS.BG });
+    ], x, y, 1, { G: FC_COLORS.GRASS, L: FC_COLORS.GRASS_LIGHT, F: FC_COLORS.GRASS_DARK });
   },
 
   drawDirt(ctx, x, y) {
     PixelSprites.drawSpriteFromGrid(ctx, [
-      'SSSSSSSSSSSSSSSS',
-      'SSSSSSSSSSSSSSSS',
-      'SSSSSSSSSSSSSSSS',
-      'SSSSSSSSSSSSSSSS',
-      'SSSSSSSSSSSSSSSS',
-      'SSSSSSSSSSSSSSSS',
-      'SSSSSSSSSSSSSSSS',
-      'SSSSSSSSSSSSSSSS',
-      'SSSSSSSSSSSSSSSS',
-      'SSSSSSSSSSSSSSSS',
-      'SSSSSSSSSSSSSSSS',
-      'SSSSSSSSSSSSSSSS',
-      'SSSSSSSSSSSSSSSS',
-      'SSSSSSSSSSSSSSSS',
-      'SSSSSSSSSSSSSSSS',
-      'SSSSSSSSSSSSSSSS'
-    ], x, y, 1, { S: FC_COLORS.ROAD });
+      'DDDDDDDDDDDDDDDD',
+      'DDDDDDDDDDDDDDDD',
+      'DDDDDDDDDDDDDDDD',
+      'DDDDDDDDDDDDDDDD',
+      'DDDDDDDDDDDDDDDD',
+      'DDDDDDDDDDDDDDDD',
+      'DDDDDDDDDDDDDDDD',
+      'DDDDDDDDDDDDDDDD',
+      'DDDDDDDDDDDDDDDD',
+      'DDDDDDDDDDDDDDDD',
+      'DDDDDDDDDDDDDDDD',
+      'DDDDDDDDDDDDDDDD',
+      'DDDDDDDDDDDDDDDD',
+      'DDDDDDDDDDDDDDDD',
+      'DDDDDDDDDDDDDDDD',
+      'DDDDDDDDDDDDDDDD'
+    ], x, y, 1, { D: FC_COLORS.ROAD });
   },
 
   drawWater(ctx, x, y, time) {
-    const wave = time % 2 === 0 ? 'W' : 'w';
+    const colors = {
+      '0': FC_COLORS.WATER,
+      '1': FC_COLORS.WATER_LIGHT,
+      '2': FC_COLORS.WATER,
+      '3': FC_COLORS.WATER_DARK
+    };
     PixelSprites.drawSpriteFromGrid(ctx, [
-      'WWWWWWWWWWWWWWWW',
-      'WWWWWWWWWWWWWWWW',
-      'WWWWWWWWWWWWWWWW',
-      'WWWWWWWWWWWWWWWW',
-      'WWWWWWWWWWWWWWWW',
-      'WWWWWWWWWWWWWWWW',
-      'WWWWWWWWWWWWWWWW',
-      'WWWWWWWWWWWWWWWW',
-      'WWWWWWWWWWWWWWWW',
-      'WWWWWWWWWWWWWWWW',
-      'WWWWWWWWWWWWWWWW',
-      'WWWWWWWWWWWWWWWW',
-      'WWWWWWWWWWWWWWWW',
-      'WWWWWWWWWWWWWWWW',
-      'WWWWWWWWWWWWWWWW',
-      'WWWWWWWWWWWWWWWW'
-    ], x, y, 1, { W: FC_COLORS.WATER, w: FC_COLORS.WATER });
+      '0011110011110011',
+      '0011110011110011',
+      '2233332233332233',
+      '2233332233332233',
+      '0011110011110011',
+      '0011110011110011',
+      '2233332233332233',
+      '2233332233332233',
+      '0011110011110011',
+      '0011110011110011',
+      '2233332233332233',
+      '2233332233332233',
+      '0011110011110011',
+      '0011110011110011',
+      '2233332233332233',
+      '2233332233332233'
+    ], x, y, 1, colors);
   },
 
   drawRoad(ctx, x, y) {
     PixelSprites.drawSpriteFromGrid(ctx, [
-      'SSSSSSSSSSSSSSSS',
-      'SSSSSSSSSSSSSSSS',
-      'SSSSSSSSSSSSSSSS',
-      'SSSSSSSSSSSSSSSS',
-      'SSSSSSSSSSSSSSSS',
-      'SSSSSSSSSSSSSSSS',
-      'SSSSSSSSSSSSSSSS',
-      'SSSSSSSSSSSSSSSS',
-      'SSSSSSSSSSSSSSSS',
-      'SSSSSSSSSSSSSSSS',
-      'SSSSSSSSSSSSSSSS',
-      'SSSSSSSSSSSSSSSS',
-      'SSSSSSSSSSSSSSSS',
-      'SSSSSSSSSSSSSSSS',
-      'SSSSSSSSSSSSSSSS',
-      'SSSSSSSSSSSSSSSS'
-    ], x, y, 1, { S: FC_COLORS.ROAD });
+      'DDDDDDDDDDDDDDDD',
+      'DDDDDDDDDDDDDDDD',
+      'DDDDDDDDDDDDDDDD',
+      'DDDDDDDDDDDDDDDD',
+      'DDDDDDDDDDDDDDDD',
+      'DDDDDDDDDDDDDDDD',
+      'DDDDDDDDDDDDDDDD',
+      'DDDDDDDDDDDDDDDD',
+      'DDDDDDDDDDDDDDDD',
+      'DDDDDDDDDDDDDDDD',
+      'DDDDDDDDDDDDDDDD',
+      'DDDDDDDDDDDDDDDD',
+      'DDDDDDDDDDDDDDDD',
+      'DDDDDDDDDDDDDDDD',
+      'DDDDDDDDDDDDDDDD',
+      'DDDDDDDDDDDDDDDD'
+    ], x, y, 1, { D: FC_COLORS.ROAD });
   },
 
   drawWall(ctx, x, y) {
     PixelSprites.drawSpriteFromGrid(ctx, [
-      'BBBBBBBBBBBBBBBB',
-      'BBBBBBBBBBBBBBBB',
-      'SSSSSSSSSSSSSSSS',
-      'BBBBBBBBBBBBBBBB',
-      'BBBBBBBBBBBBBBBB',
-      'SSSSSSSSSSSSSSSS',
-      'BBBBBBBBBBBBBBBB',
-      'BBBBBBBBBBBBBBBB',
-      'SSSSSSSSSSSSSSSS',
-      'BBBBBBBBBBBBBBBB',
-      'BBBBBBBBBBBBBBBB',
-      'SSSSSSSSSSSSSSSS',
-      'BBBBBBBBBBBBBBBB',
-      'BBBBBBBBBBBBBBBB',
-      'SSSSSSSSSSSSSSSS',
-      'BBBBBBBBBBBBBBBB'
-    ], x, y, 1, { B: FC_COLORS.BUILDING, S: FC_COLORS.ROAD });
+      'WWWWWWWWWWWWWWWW',
+      'WLLLWLLLWLLLWLLW',
+      'WWWWWWWWWWWWWWWW',
+      'WWWWWWWWWWWWWWWW',
+      'WLLLWLLLWLLLWLLW',
+      'WWWWWWWWWWWWWWWW',
+      'WWWWWWWWWWWWWWWW',
+      'WLLLWLLLWLLLWLLW',
+      'WWWWWWWWWWWWWWWW',
+      'WWWWWWWWWWWWWWWW',
+      'WLLLWLLLWLLLWLLW',
+      'WWWWWWWWWWWWWWWW',
+      'WWWWWWWWWWWWWWWW',
+      'WLLLWLLLWLLLWLLW',
+      'WWWWWWWWWWWWWWWW',
+      'WWWWWWWWWWWWWWWW'
+    ], x, y, 1, { W: FC_COLORS.BUILDING, L: FC_COLORS.BUILDING_LIGHT });
   },
 
   drawFloor(ctx, x, y) {
     PixelSprites.drawSpriteFromGrid(ctx, [
       'SSSSSSSSSSSSSSSS',
       'SSSSSSSSSSSSSSSS',
-      'SSBBBBBBSSSSSSSS',
-      'SSBBBBBBSSSSSSSS',
-      'SSBBBBBBSSSSSSSS',
-      'SSBBBBBBSSSSSSSS',
       'SSSSSSSSSSSSSSSS',
       'SSSSSSSSSSSSSSSS',
-      'SSSSSSSSSSBBBBSS',
-      'SSSSSSSSSSBBBBSS',
-      'SSSSSSSSSSBBBBSS',
-      'SSSSSSSSSSBBBBSS',
+      'SSSSSSSSSSSSSSSS',
+      'SSSSSSSSSSSSSSSS',
+      'SSSSSSSSSSSSSSSS',
+      'SSSSSSSSSSSSSSSS',
+      'SSSSSSSSSSSSSSSS',
+      'SSSSSSSSSSSSSSSS',
+      'SSSSSSSSSSSSSSSS',
+      'SSSSSSSSSSSSSSSS',
       'SSSSSSSSSSSSSSSS',
       'SSSSSSSSSSSSSSSS',
       'SSSSSSSSSSSSSSSS',
       'SSSSSSSSSSSSSSSS'
-    ], x, y, 1, { S: FC_COLORS.ROAD, B: FC_COLORS.BUILDING });
+    ], x, y, 1, { S: FC_COLORS.ROAD_DARK });
   },
 
   drawDoor(ctx, x, y) {
     PixelSprites.drawSpriteFromGrid(ctx, [
-      'BBBBBBBBBBBBBBBB',
-      'BBBBBBBBBBBBBBBB',
-      'BBBBBBBBBBBBBBBB',
-      'BBBBBBBBBBBBBBBB',
-      'BBBBBBBBBBBBBBBB',
-      'BBBBBBBBBBBBBBBB',
-      'BBBBKKKKKBBBBBBB',
-      'BBBBKKKKKBBBBBBB',
-      'BBBBKKKKKBBBBBBB',
-      'BBBBKKKKKBBBBBBB',
-      'BBBBKKKKKBBBBBBB',
-      'BBBBKKKKKBBBBBBB',
-      'BBBBKKKKKBBBBBBB',
-      'BBBBBBBBBBBBBBBB',
-      'BBBBBBBBBBBBBBBB',
-      'BBBBBBBBBBBBBBBB'
-    ], x, y, 1, { B: FC_COLORS.BUILDING, K: FC_COLORS.BLACK });
+      'WWWWWWWWWWWWWWWW',
+      'WWWWWWWWWWWWWWWW',
+      'WWWWWWWWWWWWWWWW',
+      'WWWWWWWWWWWWWWWW',
+      'WWWWKKKKKKKKKKWW',
+      'WWWWKKKKKKKKKKWW',
+      'WWWWKKKKKKKKKKWW',
+      'WWWWKKKKKKKKKKWW',
+      'WWWWKKKKKKKKKKWW',
+      'WWWWKKKKKKKKKKWW',
+      'WWWWKKKKKKKKKKWW',
+      'WWWWKKKKKKKKKKWW',
+      'WWWWKKKKKKKKKKWW',
+      'WWWWKKKKKKKKKKWW',
+      'WWWWKKKKKKKKKKWW',
+      'WWWWWWWWWWWWWWWW'
+    ], x, y, 1, { W: FC_COLORS.BUILDING, K: FC_COLORS.BLACK });
   },
 
   drawRoof(ctx, x, y) {
     PixelSprites.drawSpriteFromGrid(ctx, [
-      'BBBBBBBBBBBBBBBB',
-      'BBBBBBBBBBBBBBBB',
-      'BBBBBBBBBBBBBBBB',
-      'BBBBBBBBBBBBBBBB',
-      'BBBBBBBBBBBBBBBB',
-      'BBBBBBBBBBBBBBBB',
-      'BBBBBBBBBBBBBBBB',
-      'BBBBBBBBBBBBBBBB',
-      'BBBBBBBBBBBBBBBB',
-      'BBBBBBBBBBBBBBBB',
-      'BBBBBBBBBBBBBBBB',
-      'BBBBBBBBBBBBBBBB',
-      'BBBBBBBBBBBBBBBB',
-      'BBBBBBBBBBBBBBBB',
-      'BBBBBBBBBBBBBBBB',
-      'BBBBBBBBBBBBBBBB'
-    ], x, y, 1, { B: FC_COLORS.BUILDING });
+      'RRRRRRRRRRRRRRRR',
+      'RLLRRLLRRLLRRLLR',
+      'RRRRRRRRRRRRRRRR',
+      'RLLRRLLRRLLRRLLR',
+      'RRRRRRRRRRRRRRRR',
+      'RLLRRLLRRLLRRLLR',
+      'RRRRRRRRRRRRRRRR',
+      'RLLRRLLRRLLRRLLR',
+      'RRRRRRRRRRRRRRRR',
+      'RLLRRLLRRLLRRLLR',
+      'RRRRRRRRRRRRRRRR',
+      'RLLRRLLRRLLRRLLR',
+      'RRRRRRRRRRRRRRRR',
+      'RLLRRLLRRLLRRLLR',
+      'RRRRRRRRRRRRRRRR',
+      'RLLRRLLRRLLRRLLR'
+    ], x, y, 1, { R: FC_COLORS.ROOF, L: FC_COLORS.ROOF_LIGHT });
   },
 
   drawTree(ctx, x, y) {
     PixelSprites.drawSpriteFromGrid(ctx, [
-      '      GGGG      ',
-      '     GGGGGG     ',
-      '    GGGGGGGG    ',
-      '   GGGGGGGGGG   ',
-      '  GGGGGGGGGGGG  ',
-      ' GGGGGGGGGGGGGG ',
-      'GGGGGGGGGGGGGGGG',
-      ' GGGGGGGGGGGGGG ',
-      '  GGGGGGGGGGGG  ',
-      '      TTTT      ',
-      '      TTTT      ',
-      '      TTTT      ',
-      '      TTTT      ',
-      '      TTTT      ',
-      '      TTTT      ',
-      '      TTTT      '
-    ], x, y, 1, { G: FC_COLORS.GRASS, T: FC_COLORS.BUILDING });
+      '    LLLLLL    ',
+      '   LLLLLLLL   ',
+      '  LLLLLLLLLL  ',
+      ' LLLLLLLLLLLL ',
+      'LLLLLLLLLLLLLLL',
+      'LLLLLLLLLLLLLLL',
+      ' LLLLLLLLLLLLL ',
+      '  LLLLLLLLLL  ',
+      '    LLLLLL    ',
+      '     TTTT     ',
+      '     TTTT     ',
+      '     TTTT     ',
+      '     TTTT     ',
+      '     TTTT     ',
+      '     TTTT     ',
+      '     TTTT     '
+    ], x, y, 1, { L: FC_COLORS.TREE_LEAVES, T: FC_COLORS.TREE_TRUNK });
   },
 
   drawFence(ctx, x, y) {
     PixelSprites.drawSpriteFromGrid(ctx, [
+      'WWWWWWWWWWWWWWWW',
+      'W  W  W  W  W  W',
+      'WWWWWWWWWWWWWWWW',
+      ' W  W  W  W  W W',
+      'WWWWWWWWWWWWWWWW',
+      'W  W  W  W  W  W',
+      'WWWWWWWWWWWWWWWW',
+      ' W  W  W  W  W W',
+      'WWWWWWWWWWWWWWWW',
+      'W  W  W  W  W  W',
+      'WWWWWWWWWWWWWWWW',
+      ' W  W  W  W  W W',
+      'WWWWWWWWWWWWWWWW',
+      'W  W  W  W  W  W',
+      'WWWWWWWWWWWWWWWW',
+      ' W  W  W  W  W W'
+    ], x, y, 1, { W: FC_COLORS.BUILDING });
+  },
+
+  drawStone(ctx, x, y) {
+    PixelSprites.drawSpriteFromGrid(ctx, [
       'SSSSSSSSSSSSSSSS',
-      'SBBSSBBSSBBSSBB',
       'SSSSSSSSSSSSSSSS',
       'SSSSSSSSSSSSSSSS',
-      'SBBSSBBSSBBSSBB',
       'SSSSSSSSSSSSSSSS',
       'SSSSSSSSSSSSSSSS',
-      'SBBSSBBSSBBSSBB',
       'SSSSSSSSSSSSSSSS',
       'SSSSSSSSSSSSSSSS',
-      'SBBSSBBSSBBSSBB',
       'SSSSSSSSSSSSSSSS',
       'SSSSSSSSSSSSSSSS',
-      'SBBSSBBSSBBSSBB',
+      'SSSSSSSSSSSSSSSS',
+      'SSSSSSSSSSSSSSSS',
+      'SSSSSSSSSSSSSSSS',
+      'SSSSSSSSSSSSSSSS',
+      'SSSSSSSSSSSSSSSS',
       'SSSSSSSSSSSSSSSS',
       'SSSSSSSSSSSSSSSS'
-    ], x, y, 1, { S: FC_COLORS.BG, B: FC_COLORS.BUILDING });
+    ], x, y, 1, { S: FC_COLORS.STONE });
+  },
+
+  drawFlower(ctx, x, y) {
+    const flowerType = (x + y) % 2;
+    
+    const patterns = [
+      [
+        'GGGGGGGGGGGGGGGG',
+        'GGGGGGGGGGGGGGGG',
+        'GGGGFGGGGGGGGGGG',
+        'GGGGGGGGGGGGGGGG',
+        'GGGGGGGGGGGGGGGG',
+        'GGGGGGGGGGGGGGGG',
+        'GGGGGGGGGGGGGGGG',
+        'GGGGGGGGGGGGGGGG',
+        'GGGGGGGGGGGGGGGG',
+        'GGGGGGGGGGGGGGGG',
+        'GGGGGGGGGGGGGGGG',
+        'GGGGFGGGGGGGGGGG',
+        'GGGGGGGGGGGGGGGG',
+        'GGGGGGGGGGGGGGGG',
+        'GGGGGGGGGGGGGGGG',
+        'GGGGGGGGGGGGGGGG'
+      ],
+      [
+        'GGGGGGGGGGGGGGGG',
+        'GGGGGGGGGGGGGGGG',
+        'GGGGGGGGGGGGGGGG',
+        'GGGGGGGGGGGGGGGG',
+        'GGGGFGGGGGGGGGGG',
+        'GGGGGGGGGGGGGGGG',
+        'GGGGGGGGGGGGGGGG',
+        'GGGGGGGGGGGGGGGG',
+        'GGGGGGGGGGGGGGGG',
+        'GGGGGGGGGGGGGGGG',
+        'GGGGGGGGGGGGGGGG',
+        'GGGGGGGGGGGGGGGG',
+        'GGGGGGGGGGGGGGGG',
+        'GGGGFGGGGGGGGGGG',
+        'GGGGGGGGGGGGGGGG',
+        'GGGGGGGGGGGGGGGG'
+      ]
+    ];
+    
+    PixelSprites.drawSpriteFromGrid(ctx, patterns[flowerType], x, y, 1, { G: FC_COLORS.GRASS, F: flowerType === 0 ? FC_COLORS.YELLOW : FC_COLORS.RED });
+  },
+
+  drawPath(ctx, x, y) {
+    PixelSprites.drawSpriteFromGrid(ctx, [
+      'DDDDDDDDDDDDDDDD',
+      'DDDDDDDDDDDDDDDD',
+      'DDDDDDDDDDDDDDDD',
+      'DDDDDDDDDDDDDDDD',
+      'DDDDDDDDDDDDDDDD',
+      'DDDDDDDDDDDDDDDD',
+      'DDDDDDDDDDDDDDDD',
+      'DDDDDDDDDDDDDDDD',
+      'DDDDDDDDDDDDDDDD',
+      'DDDDDDDDDDDDDDDD',
+      'DDDDDDDDDDDDDDDD',
+      'DDDDDDDDDDDDDDDD',
+      'DDDDDDDDDDDDDDDD',
+      'DDDDDDDDDDDDDDDD',
+      'DDDDDDDDDDDDDDDD',
+      'DDDDDDDDDDDDDDDD'
+    ], x, y, 1, { D: FC_COLORS.ROAD_LIGHT });
   },
 
   drawTile(ctx, tileType, x, y, time) {
@@ -271,6 +385,15 @@ const TileSprites = {
         break;
       case TILES.FENCE:
         this.drawFence(ctx, x, y);
+        break;
+      case TILES.STONE:
+        this.drawStone(ctx, x, y);
+        break;
+      case TILES.FLOWER:
+        this.drawFlower(ctx, x, y);
+        break;
+      case TILES.PATH:
+        this.drawPath(ctx, x, y);
         break;
       default:
         this.drawGrass(ctx, x, y);
--- src/town_map.js
+++ src/town_map.js
@@ -1,7 +1,7 @@
 import { TILES } from './tilemap.js';
 
-const MAP_WIDTH = 32;
-const MAP_HEIGHT = 24;
+const MAP_WIDTH = 40;
+const MAP_HEIGHT = 30;
 
 function createTownTiles() {
   const tiles = [];
@@ -10,48 +10,85 @@ function createTownTiles() {
     tiles.push(TILES.GRASS);
   }
 
-  for (let y = 10; y < 14; y++) {
+  for (let y = 12; y < 18; y++) {
     for (let x = 0; x < MAP_WIDTH; x++) {
       tiles[y * MAP_WIDTH + x] = TILES.ROAD;
     }
   }
 
-  for (let x = 12; x < 16; x++) {
+  for (let x = 16; x < 24; x++) {
     for (let y = 0; y < MAP_HEIGHT; y++) {
       tiles[y * MAP_WIDTH + x] = TILES.ROAD;
     }
   }
 
-  addBuilding(tiles, 2, 2, 6, 6, 'house');
-  addBuilding(tiles, 12, 2, 8, 6, 'shop');
-  addBuilding(tiles, 24, 2, 6, 6, 'lab');
-  addBuilding(tiles, 2, 16, 6, 6, 'house');
-  addBuilding(tiles, 12, 16, 8, 6, 'shop');
-  addBuilding(tiles, 24, 16, 6, 6, 'lab');
+  for (let y = 5; y < 12; y++) {
+    for (let x = 16; x < 24; x++) {
+      tiles[y * MAP_WIDTH + x] = TILES.ROAD;
+    }
+  }
 
-  addTree(tiles, 0, 8);
-  addTree(tiles, 1, 8);
-  addTree(tiles, 8, 8);
-  addTree(tiles, 9, 8);
-  addTree(tiles, 20, 8);
-  addTree(tiles, 21, 8);
-  addTree(tiles, 30, 8);
-  addTree(tiles, 31, 8);
-  addTree(tiles, 0, 16);
-  addTree(tiles, 1, 16);
-  addTree(tiles, 8, 16);
-  addTree(tiles, 9, 16);
-  addTree(tiles, 20, 16);
-  addTree(tiles, 21, 16);
-  addTree(tiles, 30, 16);
-  addTree(tiles, 31, 16);
+  for (let y = 18; y < 25; y++) {
+    for (let x = 16; x < 24; x++) {
+      tiles[y * MAP_WIDTH + x] = TILES.ROAD;
+    }
+  }
 
-  addFence(tiles, 0, 0, MAP_WIDTH, 1);
-  addFence(tiles, 0, MAP_HEIGHT - 1, MAP_WIDTH, 1);
-  addFence(tiles, 0, 0, 1, MAP_HEIGHT);
-  addFence(tiles, MAP_WIDTH - 1, 0, 1, MAP_HEIGHT);
+  addBuilding(tiles, 3, 3, 10, 8, 'shop');
+  addBuilding(tiles, 27, 3, 10, 8, 'house');
+  addBuilding(tiles, 3, 19, 10, 8, 'lab');
+  addBuilding(tiles, 27, 19, 10, 8, 'bar');
+  addBuilding(tiles, 16, 3, 10, 8, 'hospital');
+  addBuilding(tiles, 16, 19, 10, 8, 'garage');
+
+  for (let x = 2; x < MAP_WIDTH - 2; x++) {
+    addDecor(tiles, x, 2);
+    addDecor(tiles, x, 11);
+    addDecor(tiles, x, 18);
+    addDecor(tiles, x, 27);
+  }
+
+  for (let y = 3; y < MAP_HEIGHT - 3; y++) {
+    addDecor(tiles, 2, y);
+    addDecor(tiles, 37, y);
+  }
+
+  for (let i = 0; i < 10; i++) {
+    const x = 3 + Math.floor(Math.random() * 34);
+    const y = 3 + Math.floor(Math.random() * 24);
+    if (!isBuildingArea(x, y)) {
+      addTree(tiles, x, y);
+    }
+  }
+
+  for (let i = 0; i < 15; i++) {
+    const x = 3 + Math.floor(Math.random() * 34);
+    const y = 3 + Math.floor(Math.random() * 24);
+    if (!isBuildingArea(x, y) && !isRoad(x, y)) {
+      addFlower(tiles, x, y);
+    }
+  }
+
+  addPath(tiles, 7, 10, 7, 12);
+  addPath(tiles, 32, 10, 32, 12);
+  addPath(tiles, 7, 18, 7, 18);
+  addPath(tiles, 32, 18, 32, 18);
 
   return tiles;
+}
+
+function isBuildingArea(x, y) {
+  return (x >= 3 && x < 13 && y >= 3 && y < 11) ||
+         (x >= 27 && x < 37 && y >= 3 && y < 11) ||
+         (x >= 3 && x < 13 && y >= 19 && y < 27) ||
+         (x >= 27 && x < 37 && y >= 19 && y < 27) ||
+         (x >= 16 && x < 26 && y >= 3 && y < 11) ||
+         (x >= 16 && x < 26 && y >= 19 && y < 27);
+}
+
+function isRoad(x, y) {
+  return (y >= 12 && y < 18) ||
+         (x >= 16 && x < 24);
 }
 
 function addBuilding(tiles, startX, startY, width, height, type) {
@@ -75,11 +112,27 @@ function addTree(tiles, x, y) {
   }
 }
 
-function addFence(tiles, startX, startY, width, height) {
-  for (let y = startY; y < startY + height; y++) {
-    for (let x = startX; x < startX + width; x++) {
+function addFlower(tiles, x, y) {
+  if (x >= 0 && x < MAP_WIDTH && y >= 0 && y < MAP_HEIGHT) {
+    tiles[y * MAP_WIDTH + x] = TILES.FLOWER;
+  }
+}
+
+function addDecor(tiles, x, y) {
+  if (x >= 0 && x < MAP_WIDTH && y >= 0 && y < MAP_HEIGHT) {
+    if (Math.random() > 0.5) {
+      tiles[y * MAP_WIDTH + x] = TILES.TREE;
+    } else {
+      tiles[y * MAP_WIDTH + x] = TILES.FLOWER;
+    }
+  }
+}
+
+function addPath(tiles, startX, startY, endX, endY) {
+  for (let x = Math.min(startX, endX); x <= Math.max(startX, endX); x++) {
+    for (let y = Math.min(startY, endY); y <= Math.max(startY, endY); y++) {
       if (x >= 0 && x < MAP_WIDTH && y >= 0 && y < MAP_HEIGHT) {
-        tiles[y * MAP_WIDTH + x] = TILES.FENCE;
+        tiles[y * MAP_WIDTH + x] = TILES.PATH;
       }
     }
   }
@@ -94,36 +147,26 @@ function createCollisionMap() {
 
   for (let y = 0; y < MAP_HEIGHT; y++) {
     for (let x = 0; x < MAP_WIDTH; x++) {
-      const idx = y * MAP_WIDTH + x;
       if (x === 0 || x === MAP_WIDTH - 1 || y === 0 || y === MAP_HEIGHT - 1) {
-        collisions[idx] = true;
+        collisions[y * MAP_WIDTH + x] = true;
       }
     }
   }
 
-  markBuildingCollision(collisions, 2, 2, 6, 6);
-  markBuildingCollision(collisions, 12, 2, 8, 6);
-  markBuildingCollision(collisions, 24, 2, 6, 6);
-  markBuildingCollision(collisions, 2, 16, 6, 6);
-  markBuildingCollision(collisions, 12, 16, 8, 6);
-  markBuildingCollision(collisions, 24, 16, 6, 6);
+  markBuildingCollision(collisions, 3, 3, 10, 8);
+  markBuildingCollision(collisions, 27, 3, 10, 8);
+  markBuildingCollision(collisions, 3, 19, 10, 8);
+  markBuildingCollision(collisions, 27, 19, 10, 8);
+  markBuildingCollision(collisions, 16, 3, 10, 8);
+  markBuildingCollision(collisions, 16, 19, 10, 8);
 
-  markTreeCollision(collisions, 0, 8);
-  markTreeCollision(collisions, 1, 8);
-  markTreeCollision(collisions, 8, 8);
-  markTreeCollision(collisions, 9, 8);
-  markTreeCollision(collisions, 20, 8);
-  markTreeCollision(collisions, 21, 8);
-  markTreeCollision(collisions, 30, 8);
-  markTreeCollision(collisions, 31, 8);
-  markTreeCollision(collisions, 0, 16);
-  markTreeCollision(collisions, 1, 16);
-  markTreeCollision(collisions, 8, 16);
-  markTreeCollision(collisions, 9, 16);
-  markTreeCollision(collisions, 20, 16);
-  markTreeCollision(collisions, 21, 16);
-  markTreeCollision(collisions, 30, 16);
-  markTreeCollision(collisions, 31, 16);
+  for (let i = 0; i < 10; i++) {
+    const x = 3 + Math.floor(Math.random() * 34);
+    const y = 3 + Math.floor(Math.random() * 24);
+    if (!isBuildingArea(x, y)) {
+      markTreeCollision(collisions, x, y);
+    }
+  }
 
   return collisions;
 }
@@ -147,15 +190,15 @@ function markTreeCollision(collisions, x, y) {
 }
 
 const ENTRANCES = [
-  { x: 4, y: 7, target: 'home' },
-  { x: 15, y: 7, target: 'shop' },
-  { x: 26, y: 7, target: 'lab' },
-  { x: 4, y: 21, target: 'garage' },
-  { x: 15, y: 21, target: 'hospital' },
-  { x: 26, y: 21, target: 'hunter' }
+  { x: 7, y: 10, target: 'shop' },
+  { x: 31, y: 10, target: 'house' },
+  { x: 7, y: 26, target: 'lab' },
+  { x: 31, y: 26, target: 'bar' },
+  { x: 19, y: 10, target: 'hospital' },
+  { x: 19, y: 26, target: 'garage' }
 ];
 
-const START_POSITION = { x: 14, y: 12 };
+const START_POSITION = { x: 19, y: 15 };
 
 export const TOWN_MAP = {
   tiles: createTownTiles(),
